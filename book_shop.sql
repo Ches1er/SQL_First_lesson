ad47dc420745a195a905c6b8b8dea0776bf5343e
Ivan <chester940@gmail.com>
2018-12-29 10:37:24 +0200

    table insert data

--- book_shop.sql
+++ book_shop.sql
@@ -4,17 +4,47 @@ CREATE TABLE `clients` (`id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `client_surname` VARCHAR(255) NOT NULL
 );
 CREATE TABLE `clients_discount` (`client_id` INT UNSIGNED NOT NULL PRIMARY KEY,
-`discount` DECIMAL(3,2) NOT NULL
+`discount` FLOAT NOT NULL
 );
 CREATE TABLE `books` (`id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `book_name` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `stock` (`book_id` INT UNSIGNED NOT NULL PRIMARY KEY,
+CREATE TABLE `stock` (`book_id` INT UNSIGNED NOT NULL,
 `stock_qty` SMALLINT UNSIGNED NOT NULL
 );
-CREATE TABLE `sells` (`book_id` INT UNSIGNED NOT NULL PRIMARY KEY,
+CREATE TABLE `sells` (`book_id` INT UNSIGNED NOT NULL,
 `sells_qty` SMALLINT UNSIGNED NOT NULL
 );
-CREATE TABLE `book_discount` (`book_id` INT UNSIGNED NOT NULL PRIMARY KEY,
-`discount` DECIMAL(3,2) NOT NULL
+CREATE TABLE `book_discount` (`book_id` INT UNSIGNED NOT NULL,
+`discount` FLOAT NOT NULL
 );
+
+INSERT INTO `clients` (`client_name`,`client_surname`) VALUES ('petya','petrov'),
+('ivan','ivanov'),
+('fedor','fedorov');
+
+INSERT INTO `clients_discount` (`client_id`,`discount`) VALUES ('1','0.9'),
+('ivan','0.95'),
+('fedor','0.96');
+
+INSERT INTO `books` (`book_name`) VALUES ('Незнайка'),
+('Война и мир'),
+('Преступление и наказание'),
+('Колобок');
+
+INSERT INTO `stock` (`book_id`,`stock_qty`) VALUES ('1','10'),
+('2','30'),
+('3','40'),
+('4','56');
+
+INSERT INTO `sells` (`book_id`,`sells_qty`) VALUES ('1','5'),
+('2','4'),
+('3','15'),
+('4','20');
+
+INSERT INTO `book_discount` (`book_id`,`discount`) VALUES ('1','0.9'),
+('2','0.95'),
+('3','0.96'),
+('4','0.96')
+;
+
