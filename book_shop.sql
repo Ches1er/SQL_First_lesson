cf44db9a730c46785cec16d686c92bb5e1cd055e
Ivan <chester940@gmail.com>
2019-01-09 16:03:27 +0200

    111

--- book_shop.sql
+++ book_shop.sql
@@ -21,30 +21,47 @@ CREATE TABLE `book_discount` (`book_id` INT UNSIGNED NOT NULL,
 
 INSERT INTO `clients` (`client_name`,`client_surname`) VALUES ('petya','petrov'),
 ('ivan','ivanov'),
-('fedor','fedorov');
+('fedor','fedorov')
+;
 
 INSERT INTO `clients_discount` (`client_id`,`discount`) VALUES ('1','0.9'),
 ('ivan','0.95'),
-('fedor','0.96');
+('fedor','0.96')
+;
 
 INSERT INTO `books` (`book_name`) VALUES ('Незнайка'),
 ('Война и мир'),
 ('Преступление и наказание'),
-('Колобок');
+('Колобок'),
+('Финансист'),('Властелин колец')
+;
 
 INSERT INTO `stock` (`book_id`,`stock_qty`) VALUES ('1','10'),
 ('2','30'),
 ('3','40'),
-('4','56');
+('4','56'),
+('5','10'),
+('6','42')
+;
 
 INSERT INTO `sells` (`book_id`,`sells_qty`) VALUES ('1','5'),
 ('2','4'),
 ('3','15'),
-('4','20');
+('4','20'),
+('5','5'),
+('6','12')
+;
 
 INSERT INTO `book_discount` (`book_id`,`discount`) VALUES ('1','0.9'),
 ('2','0.95'),
 ('3','0.96'),
-('4','0.96')
+('4','0.96'),
+('5','0.98'),
+('6','0.93'),
 ;
+
+SELECT `book_name` FROM `books` WHERE `id` IN
+(SELECT `book_id` FROM `stock` ORDER BY `stock_qty` DESC LIMIT 1);
+
+
 
